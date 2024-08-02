2eb8cdbf2b1608c2c1f8276c1533c869047db23d
Dream-Sport-plus <dream.sport.plus@gmail.com>
2024-08-02 21:00:35 +0530

    Home Update

--- src/app/modules/landing/components/landing/landing.component.ts
+++ src/app/modules/landing/components/landing/landing.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone, inject } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
+import { NgClass } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { FooterComponent } from '../footer/footer.component';
+
+
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [MenuComponent],
+  imports: [MenuComponent, NgClass, MatCardModule, FooterComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
+  ngZone = inject(NgZone);
+  typedText = '';
+  text = 'A1 Hospital';
+  i = 0;
+  ngOnInit(): void {
+    this.ngZone.runOutsideAngular(() => {
+      this.typeText();
+    });
 
+  }
+
+  typeText(): void {
+    if (this.i < this.text.length) {
+      this.ngZone.run(() => {
+        this.typedText += this.text[this.i];
+      });
+      this.i++;
+      setTimeout(() => {
+        this.typeText();
+      }, 100);
+    } else {
+      setTimeout(() => {
+        this.eraseText();
+      }, 1000);
+    }
+  }
+
+  eraseText(): void {
+    if (this.i != 0) {
+      this.ngZone.run(() => {
+        this.typedText = this.typedText.substring(0, this.typedText.length - 1);
+      });
+      this.i--;
+      setTimeout(() => {
+        this.eraseText();
+      }, 100);
+    } else {
+      setTimeout(() => {
+        this.typeText();
+      }, 1000);
+    }
+  }
 }
